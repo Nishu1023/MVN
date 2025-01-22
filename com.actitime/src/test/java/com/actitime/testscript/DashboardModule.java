beb71e4030fb30115fff976816b2a0ae2fecdebd
nishu <nishu@Nishita>
2025-01-22 10:59:57 +0530

    Added open browser

--- com.actitime/src/test/java/com/actitime/testscript/DashboardModule.java
+++ com.actitime/src/test/java/com/actitime/testscript/DashboardModule.java
@@ -1,5 +1,16 @@
 package com.actitime.testscript;
 
-public class DashboardModule {
+import java.io.IOException;
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DashboardModule {
+	public static void main(String[] args) throws IOException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
 }
